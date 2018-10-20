9a1b605b40098f5363e3e01202aa762f25b0203d
reidfalconer <reidfalcon@gmail.com>
2018-10-20 18:12:33 +0200

    edit scripts

--- sample.py
+++ sample.py
@@ -4,17 +4,19 @@ from weather import Weather, Unit
 
 sense = SenseHat()
 while True:
+
     weather = Weather(unit=Unit.CELSIUS)
     location = weather.lookup_by_location('barcelona')
     condition = location.condition
     x = str(condition.text)
     y = str(condition.temp)
-# sense.load_image("img/balena.png")
-# time.sleep(3)
-    sense.show_message(x)
+    
+    sense.load_image("img/balena.png")
     time.sleep(3)
-    sense.show_message(y + " Degrees")
-    sense.clear()
+    
+    for event in sense.stick.get_events():
+        sense.show_message(y + " Degrees")
+        
 
 
 
