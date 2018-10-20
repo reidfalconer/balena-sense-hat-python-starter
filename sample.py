69a52a69bb779f785d5469e6ad5cef89a9401583
reidfalconer <reidfalcon@gmail.com>
2018-10-20 22:09:13 +0200

    add 8-ball

--- sample.py
+++ sample.py
@@ -2,9 +2,11 @@ from sense_hat import SenseHat
 import time
 from weather import Weather, Unit
 import datetime
-from random import choice
 
 sense = SenseHat()
+
+sense.show_message("Ask a question", scroll_speed=0.06)
+sleep(3)
 
 replies = ['Signs point to yes',
         'Without a doubt',
@@ -18,14 +20,6 @@ replies = ['Signs point to yes',
 
 while True:
 
-    weather = Weather(unit=Unit.CELSIUS)
-    location = weather.lookup_by_location('barcelona')
-    condition = location.condition
-    x = str(condition.text)
-    y = str(condition.temp)
-
-    sense.show_message("Ask a question", scroll_speed=0.06)
-    #sense.load_image("img/balena.png")
 
     x, y, z = sense.get_accelerometer_raw().values()
 
@@ -33,16 +27,20 @@ while True:
     y = abs(y)
     z = abs(z)
 
-     if x > 2 or y > 2 or z > 2 :
-         sense.show_message(choice(replies))
-         else:
-             sense.clear()
+    if x > 2 or y > 2 or z > 2 :
+        sense.show_message(choice(replies))
+    else:
+        sense.clear()
+
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location('barcelona')
+    condition = location.condition
+    x = str(condition.text)
+    y = str(condition.temp)
+
+    #sense.load_image("img/balena.png")
 
     for event in sense.stick.get_events():
         sense.show_message(y + " Degrees")
         sense.show_message(x)
-
-    
-
-   
 
