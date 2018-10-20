d8db491bf8335824b54d8b5340bdf8397547f31b
reidfalconer <reidfalcon@gmail.com>
2018-10-20 15:17:27 +0200

    Change text

--- sample.py
+++ sample.py
@@ -6,6 +6,6 @@ sense = SenseHat()
 while True:
     sense.load_image("img/balena.png")
     time.sleep(10)
-    sense.show_message("balena")
+    sense.show_message("Dominc Walsh is a prawn")
     time.sleep(10)
     sense.clear()
